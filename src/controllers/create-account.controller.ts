2d26a107547555186837da7566e1b5f8aaeab746
HugoSalvador <hugossalvador1241@gmail.com>
2025-01-27 14:38:57 -0300

    Created a bodySchema account, to validate the body requisition Created pipes to treat error warnings in zod Created a env validation schema

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,47 @@
+import { Body, ConflictException, Controller, HttpCode, Post, UsePipes } from "@nestjs/common"
+import { PrismaService } from "src/prisma/prisma.service"
+import { hash } from "bcryptjs"
+import { z } from "zod"
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe"
+
+const createAccoutBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string()
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccoutBodySchema>
+
+@Controller('/accounts')
+export class CreateAccountController {
+    constructor(private prisma: PrismaService) {}
+
+    @Post()
+    @HttpCode(201)
+    @UsePipes(new ZodValidationPipe(createAccoutBodySchema))
+    async handle(@Body() body: CreateAccountBodySchema) {
+        const { name, email, password} = body
+
+        const userWithSameEmail = await this.prisma.user.findUnique({
+            where: {
+                email,
+            }
+        })
+
+        if (userWithSameEmail) {
+            throw new ConflictException(
+                'User with same e-mail address already exist'
+            )
+        }
+
+        const hashedPassword = await hash(password, 8)
+
+        await this.prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        })
+    }
+}
