90b49a6fea17a07b01d3590251d23babea726091
hgSalvador <hugossalvador1241@gmail.com>
2025-04-22 17:53:52 -0300

    Created a edit-answer controller Created a e2e test for edit-answer controller

--- src/infra/http/controllers/edit-answer.controller.e2e-spec.ts
+++ src/infra/http/controllers/edit-answer.controller.e2e-spec.ts
@@ -5,20 +5,22 @@ import { INestApplication } from "@nestjs/common"
 import { JwtService } from "@nestjs/jwt"
 import { Test } from "@nestjs/testing"
 import request from 'supertest'
+import { AnswerFactory } from "test/factories/make-answer"
 import { QuestionFactory } from "test/factories/make-question"
 import { StudentFactory } from "test/factories/make-student"
 
-describe('Create question (E2E)', () => {
+describe('Edit answer (E2E)', () => {
     let app: INestApplication
     let studentFactory: StudentFactory
     let questionFactory: QuestionFactory
+    let answerFactory: AnswerFactory
     let prisma: PrismaService
     let jwt: JwtService
     
     beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
             imports: [AppModule, DatabaseModule],
-            providers: [StudentFactory, QuestionFactory]
+            providers: [StudentFactory, QuestionFactory, AnswerFactory]
         }).compile()
 
         app = moduleRef.createNestApplication()
@@ -26,12 +28,13 @@ describe('Create question (E2E)', () => {
         prisma = moduleRef.get(PrismaService)
         studentFactory = moduleRef.get(StudentFactory)
         questionFactory = moduleRef.get(QuestionFactory)
+        answerFactory = moduleRef.get(AnswerFactory)
         jwt = moduleRef.get(JwtService)
 
         await app.init()
     })
 
-    test('[POST] /questions/:questionId/anwers', async () => {
+    test('[PUT] /answers/:id', async () => {
         const user = await studentFactory.makePrismaStudent()
 
         const accessToken = jwt.sign({ sub: user.id.toString() })
@@ -40,21 +43,26 @@ describe('Create question (E2E)', () => {
             authorId: user.id
         })
         
-        const questionId = question.id.toString()
-
-        const response = await request(app.getHttpServer())
-        .post(`/questions/${questionId}/answers`)
-        .set('Authorization', `Bearer ${accessToken}`)
-        .send({
-            content: 'New answer'
+        const answer = await answerFactory.makePrismaAnswer({
+            questionId: question.id,
+            authorId: user.id
         })
 
-        expect(response.statusCode).toBe(201)
+        const answerId = answer.id.toString()
+
+        const response = await request(app.getHttpServer())
+        .put(`/answers/${answerId}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send({
+            content: 'New answer content'
+        })
+
+        expect(response.statusCode).toBe(204)
 
 
         const answerOnDatabase = await prisma.answer.findFirst({
             where: {
-                content: 'New answer'
+                content: 'New answer content'
             }
         })
 
---
+++ src/infra/http/controllers/edit-answer.controller.ts
@@ -0,0 +1,47 @@
+import { BadRequestException, Body, Controller, HttpCode, Param, Post, Put, UseGuards } from "@nestjs/common"
+import { CurrentUser } from "@/auth/current-user-decorator"
+import { JwtAuthGuard } from "@/auth/jwt-auth.guard"
+import { UserPayload } from "@/auth/jwt.strategy"
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe"
+import { z } from "zod"
+import { EditAnswerUseCase } from "@/domain/forum/application/use-cases/edit-answer"
+
+
+
+const editAnswerBodySchema = z.object({
+    content: z.string()
+})
+
+const bodyValidationPipe = new ZodValidationPipe(editAnswerBodySchema)
+
+type EditAnswerBodySchema = z.infer<typeof editAnswerBodySchema>
+
+
+@Controller('/answers/:id')
+@UseGuards(JwtAuthGuard)
+export class EditAnswerController {
+    constructor(private editAnswer: EditAnswerUseCase) {}
+
+    @Put()
+    @HttpCode(204)
+    async handle(
+        @Body(bodyValidationPipe) body:EditAnswerBodySchema,
+        @CurrentUser() user: UserPayload,
+        @Param('id') answerId: string
+    
+    ) {
+        const { content } = body
+        const userId = user.sub
+
+        const result = await this.editAnswer.execute({
+            content,
+            answerId,
+            authorId: userId,
+            attachmentsIds: []
+        })
+
+        if (result.isLeft()) {
+            throw new BadRequestException()
+        }
+    } 
+}
